77467e42f12af6a9079ebf741c17343e2131c090
emetowinner <emetowinner@gmail.com>
2020-05-12 10:00:22 +0100

    update day 2 task

---
+++ Phase-1/Python Basic 1/Day-1.py
@@ -0,0 +1,17 @@
+"""
+Good morning everyone  and welcome to day one review 😇😇
+
+
+ Target for today are: 
+
+ 1. Creat a kaggle.com account and access this https://www.kaggle.com/learn/python. For video lovers you can access this https://drive.google.com/folderview?id=1_vjP0vKxi5CSchnL7eRMyT3VYLLLi4xk and for readers refer to the PDF file I shared.
+
+ 2. Head over to the organization page and set up the task repo https://github.com/CodedLadiesInnovateTech/python-challenges and submission repo https://github.com/CodedLadiesInnovateTech/python-challenge-solutions, you will find instructions on how to do that in the README file
+
+ 3.  Register yourself here http://bit.ly/dataaboutyou
+
+ 4. Come back and reply to this message with  your name and this ✅
+
+ Happy Setting up 😎😎😎
+
+"""
---
+++ Phase-1/Python Basic 1/Day-2.py
@@ -0,0 +1,54 @@
+"""
+
+Target for today are:
+
+1. Write a Python program to print the following string in a specific format (see the output-1)
+    'Sample String : "Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high,
+     Like a diamond in the sky. Twinkle, twinkle, little star, How I wonder what you are"'
+    Tools: print function
+2. Write a Python program to get the Python version you are using
+    Tools: sys module
+3. Write a Python program to display the current date and time.
+    Sample Output :
+        Current date and time : 2014-07-05 14:34:14
+    Tools: Datetime module
+4. Write a Python program which accepts the radius of a circle from the user and compute the area.
+    Sample Output :
+        r = 1.1
+        Area = 3.8013271108436504
+    Tools: input function
+
+5. Write a Python program which accepts the user's first and last name and print them in reverse order with a space between them.
+    Tools: input function, list slicing
+
+6. Write a Python program which accepts a sequence of comma-separated numbers from user and generate a list and a tuple with those numbers.
+        Sample data : 3, 5, 7, 23
+        Output :
+        List : ['3', ' 5', ' 7', ' 23']
+        Tuple : ('3', ' 5', ' 7', ' 23')
+    Tools: input function, list, tuple
+
+
+7. Write a Python program to accept a filename from the user and print the extension of that.
+        Sample filename : abc.java
+        Output : java
+    Tools: input function, slicing
+
+
+8. Write a Python program to display the first and last colors from the following list.
+        color_list = ["Red","Green","White" ,"Black"]
+    Tools: list slicing,indexing
+
+
+9. Write a Python program to display the examination schedule. (extract the date from exam_st_date).
+        exam_st_date = (11, 12, 2014)
+        Sample Output : The examination will start from : 11 / 12 / 2014
+    Tools:slicing,indexing
+
+
+10. Write a Python program that accepts an integer (n) and computes the value of n+nn+nnn.
+        Sample value of n is 5
+        Expected Result : 615
+    Tools: input function, maths
+
+"""
