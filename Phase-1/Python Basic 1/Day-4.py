771cc0e13e568ca4ad232ebe4099095da4a6a7b5
Mbaoma Chioma (Omarrr) <49791498+Mbaoma@users.noreply.github.com>
2020-05-14 09:19:39 +0100

    Create Day-4.py

---
+++ Phase-1/Python Basic 1/Day-4.py
@@ -0,0 +1,38 @@
+"""
+
+1. Write a Python program to find whether a given number (accept from the user) is even or odd, print out an appropriate message to the user.
+
+2. Write a Python program to count the number 4 in a given list.
+
+3. Write a Python program to get the n (non-negative integer) copies of the first 2 characters of a given string. Return the n copies of the whole string if the length is less than 2.
+
+4. Write a Python program to test whether a passed letter is a vowel or not.
+
+5. Write a Python program to check whether a specified value is contained in a group of values.
+Test Data :
+3 -> [1, 5, 8, 3] : True
+-1 -> [1, 5, 8, 3] : False
+
+6. Write a Python program to create a histogram from a given list of integers.
+
+7. Write a Python program to concatenate all elements in a list into a string and return it.
+
+8. Write a Python program to print all even numbers from a given numbers list in the same order and stop the printing if any numbers that come after 237 in the sequence.
+Sample numbers list :
+numbers = [    
+    386, 462, 47, 418, 907, 344, 236, 375, 823, 566, 597, 978, 328, 615, 953, 345, 
+    399, 162, 758, 219, 918, 237, 412, 566, 826, 248, 866, 950, 626, 949, 687, 217, 
+    815, 67, 104, 58, 512, 24, 892, 894, 767, 553, 81, 379, 843, 831, 445, 742, 717, 
+    958,743, 527
+    ]
+
+9. Write a Python program to print out a set containing all the colors from color_list_1 which are not present in color_list_2.
+Test Data :
+color_list_1 = set(["White", "Black", "Red"])
+color_list_2 = set(["Red", "Green"])
+Expected Output :
+{'Black', 'White'}
+
+10. Write a Python program that will accept the base and height of a triangle and compute the area.
+
+"""
