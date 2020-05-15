f441feabe22966272fc50d4f7eb62b3bd884a67c
Mbaoma Chioma (Omarrr) <49791498+Mbaoma@users.noreply.github.com>
2020-05-15 20:42:24 +0100

    Day 6 challenge

---
+++ Phase-1/Python Basic 1/Day-6.py
@@ -0,0 +1,23 @@
+"""
+
+1. Write a Python program to check whether a file exists.
+
+2. Write a Python program to determine if a Python shell is executing in 32bit or 64bit mode on OS.
+
+3. Write a Python program to get OS name, platform and release information.
+
+4. Write a Python program to locate Python site-packages.
+
+5. Write a python program to call an external command in Python.
+
+6. Write a python program to get the path and name of the file that is currently executing.
+
+7. Write a Python program to find out the number of CPUs using.
+
+8. Write a Python program to parse a string to Float or Integer.
+
+9. Write a Python program to list all files in a directory in Python.
+
+10. Write a Python program to print without newline or space.
+
+"""
