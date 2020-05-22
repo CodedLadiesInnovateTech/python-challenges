f8bf1d8fd4dd8bd9c49d5fbdb5ef661b2d2c0243
Mbaoma Chioma (Omarrr) <49791498+Mbaoma@users.noreply.github.com>
2020-05-22 01:36:51 +0100

    Create Day-11.py

---
+++ Phase-1/Python Basic 1/Day-11.py
@@ -0,0 +1,24 @@
+"""
+
+1. Write a Python program to swap two variables.
+
+2. Write a Python program to define a string containing special characters in various forms.
+
+3. Write a Python program to get the identity of an object.
+
+4. Write a Python program to convert a byte string to a list of integers.
+
+5. Write a Python program to check whether a string is numeric.
+
+6. Write a Python program to print the current call stack.
+
+7. Write a Python program to list the special variables used within the language.
+
+8. Write a Python program to get the system time.
+Note : The system time is important for debugging, network information, random number seeds, or something as simple as program performance.
+
+9. Write a Python program to clear the screen or terminal.
+
+10. Write a Python program to get the name of the host on which the routine is running
+
+"""
