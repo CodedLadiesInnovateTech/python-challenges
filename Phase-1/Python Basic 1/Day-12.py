a154069a05d83f6d5be7a04fe7c50983977d474e
Mbaoma Chioma (Omarrr) <49791498+Mbaoma@users.noreply.github.com>
2020-05-22 19:54:14 +0100

    Create Day-12.py

---
+++ Phase-1/Python Basic 1/Day-12.py
@@ -0,0 +1,24 @@
+"""
+
+1. Write a Python program to access and print a URL's content to the console.
+
+2. Write a Python program to get system command output.
+
+3. Write a Python program to extract the filename from a given path.
+
+4. Write a Python program to get the effective group id, effective user id, real group id, a list of supplemental group ids associated with the current process.
+Note: Availability: Unix.
+
+5. Write a Python program to get the users environment.
+
+6. Write a Python program to divide a path on the extension separator.
+
+7. Write a Python program to retrieve file properties.
+
+8. Write a Python program to find path refers to a file or directory when you encounter a path name.
+
+9. Write a Python program to check if a number is positive, negative or zero.
+
+10. Write a Python program to get numbers divisible by fifteen from a list using an anonymous function.
+
+"""
