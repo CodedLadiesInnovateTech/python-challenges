c2c35d3aff228d2e62e6a6f5cc2b0ea619cda336
Mbaoma Chioma (Omarrr) <49791498+Mbaoma@users.noreply.github.com>
2020-05-27 11:05:21 +0100

    Create Day-16.py

---
+++ Phase-1/Python Basic 1/Day-16.py
@@ -0,0 +1,38 @@
+'''
+
+1. Write a python program to convert decimal to hexadecimal.
+  Sample decimal number: 30, 4
+  Expected output: 1e, 04
+
+2. Write a Python program to find the operating system name, platform and platform release date.
+  Operating system name:
+  posix
+  Platform name:
+  Linux
+  Platform release:
+  4.4.0-47-generic
+
+3. Write a Python program to determine if the python shell is executing in 32bit or 64bit mode on operating system.
+
+4. Write a Python program to check whether variable is of integer or string.
+
+5. Write a Python program to test if a variable is a list or tuple or a set.
+
+6. Write a Python program to find the location of Python module sources.
+  Operating system name:
+  posix
+  Platform name:
+  Linux
+  Platform release:
+  4.4.0-47-generic
+
+7. Write a Python function to check whether a number is divisible by another number. Accept two integers values form the user.
+
+8. Write a Python function to find the maximum and minimum numbers from a sequence of numbers.
+  Note: Do not use built-in functions.
+
+9. Write a Python function that takes a positive integer and returns the sum of the cube of all the positive integers smaller than the specified number.
+
+10. Write a Python function to find a distinct pair of numbers whose product is odd from a sequence of integer values.
+
+'''
