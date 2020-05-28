f83c8469adb29bf60677091ef9e511d752607eab
Mbaoma Chioma (Omarrr) <49791498+Mbaoma@users.noreply.github.com>
2020-05-28 18:28:02 +0100

    Create Day-1.py

---
+++ Phase-1/Python Basic 2/Day-1.py
@@ -0,0 +1,21 @@
+"""
+1. Write a Python function that takes a sequence of numbers and determines whether all the numbers are different from each other.
+
+2. Write a Python program to create all possible strings by using 'a', 'e', 'i', 'o', 'u'. Use the characters exactly once.
+
+3. Write a Python program to remove and print every third number from a list of numbers until the list becomes empty.
+
+4. Write a Python program to find unique triplets whose three elements gives the sum of zero from an array of n integers.
+
+5. Write a Python program to create the combinations of 3 digit combo.
+
+6. Write a Python program to print a long text, convert the string to a list and print all the words and their frequencies.
+
+7. Write a Python program to count the number of each character of a given text of a text file.
+
+8. Write a Python program to get the top stories from Google news.
+
+9. Write a Python program to get a list of locally installed Python modules.
+
+10. Write a Python program to display some information about the OS where the script is running.
+"""
