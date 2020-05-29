34042380f27acc79af590376e3cceb7c46c4e259
Mbaoma Chioma (Omarrr) <49791498+Mbaoma@users.noreply.github.com>
2020-05-29 17:44:21 +0100

    Create Day-18.py

---
+++ Phase-1/Python Basic 2/Day-18.py
@@ -0,0 +1,47 @@
+"""
+
+1. Write a Python program to check the sum of three elements (each from an array) from three arrays is equal to a target value. Print all those three-element combinations.
+   Sample data:
+   /*
+   X = [10, 20, 20, 20]
+   Y = [10, 20, 30, 40]
+   Z = [10, 30, 40, 20]
+   target = 70
+   */
+
+2. Write a Python program to create all possible permutations from a given collection of distinct numbers
+
+3. Write a Python program to get all possible two digit letter combinations from a digit (1 to 9) string.
+   string_maps = {
+   "1": "abc",
+   "2": "def",
+   "3": "ghi",
+   "4": "jkl",
+   "5": "mno",
+   "6": "pqrs",
+   "7": "tuv",
+   "8": "wxy",
+   "9": "z"
+   }
+
+4. Write a Python program to add two positive integers without using the '+' operator.
+Note: Use bit wise operations to add two numbers.
+
+5. Write a Python program to check the priority of the four operators (+, -, *, /).
+
+6. Write a Python program to get the third side of right angled triangle from two given sides.
+
+7. Write a Python program to get all strobogrammatic numbers that are of length n.
+A strobogrammatic number is a number whose numeral is rotationally symmetric, so that it appears the same when rotated 180 degrees. In other words, the numeral looks the same right-side up and upside down (e.g., 69, 96, 1001).
+For example,
+Given n = 2, return ["11", "69", "88", "96"].
+Given n = 3, return ['818', '111', '916', '619', '808', '101', '906', '609', '888', '181', '986', '689']
+
+8. Write a Python program to find the median among three given numbers.
+
+9. Write a Python program to find the value of n where n degrees of number 2 are written sequentially in a line without spaces.
+
+10. Write a Python program to find the number of zeros at the end of a factorial of a given positive number.
+Range of the number(n): (1 = n = 2*109).
+
+"""
