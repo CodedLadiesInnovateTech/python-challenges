0698fef4b53054129b31110191843565c0215d4f
Mbaoma Chioma (Omarrr) <49791498+Mbaoma@users.noreply.github.com>
2020-05-31 13:12:18 +0100

    Create Day-20.py

---
+++ Phase-1/Python Basic 2/Day-20.py
@@ -0,0 +1,91 @@
+"""
+1. Write a Python program to count the number of carry operations for each of a set of addition problems.
+According to Wikipedia " In elementary arithmetic, a carry is a digit that is transferred from one column of digits to another column of more significant digits. It is part of the standard algorithm to add numbers together by starting with the rightmost digits and working to the left. For example, when 6 and 7 are added to make 13, the "3" is written to the same column and the "1" is carried to the left".
+
+2. Write a python program to find heights of the top three building in descending order from eight given buildings.
+Input:
+0 = height of building (integer) = 10,000
+Input the heights of eight buildings:
+25
+35
+15
+16
+30
+45
+37
+39
+Heights of the top three buildings:
+45
+39
+37
+
+3. Write a Python program to compute the digit number of sum of two given integers.
+Input:
+Each test case consists of two non-negative integers x and y which are separated by a space in a line.
+0 = x, y = 1,000,000
+Input two integers(a b):
+5 7
+Sum of two integers a and b.:
+2
+
+4. Write a Python program to check whether three given lengths (integers) of three sides form a right triangle. Print "Yes" if the given sides form a right triangle otherwise print "No".
+Input:
+Integers separated by a single space.
+1 = length of the side = 1,000
+Input three integers(sides of a triangle)
+8 6 7
+No
+
+5. Write a Python program which solve the equation:
+ax+by=c
+dx+ey=f
+Print the values of x, y where a, b, c, d, e and f are given.
+Input:
+a,b,c,d,e,f separated by a single space.
+(-1,000 = a,b,c,d,e,f = 1,000)
+Input the value of a, b, c, d, e, f:
+5 8 6 7 9 4
+Values of x and y:
+-2.000 2.000
+
+6. Write a Python program to compute the amount of the debt in n months. The borrowing amount is $100,000 and the loan adds 5% interest of the debt and rounds it to the nearest 1,000 above month by month.
+Input:
+An integer n (0 = n = 100)
+Input number of months:
+7 Amount of debt: $144000
+
+7. Write a Python program which reads an integer n and find the number of combinations of a,b,c and d (0 = a,b,c,d = 9) where (a + b + c + d) will be equal to n.
+Input:
+n (1 = n = 50)
+Input the number(n):
+15
+Number of combinations: 592
+
+8. Write a Python program to print the number of prime numbers which are less than or equal to a given integer.
+Input:
+n (1 = n = 999,999)
+Input the number(n):
+35
+Number of prime numbers which are less than or equal to n.: 11
+
+9. Write a program to compute the radius and the central coordinate (x, y) of a circle which is constructed by three given points on the plane surface.
+Input:
+x1, y1, x2, y2, x3, y3 separated by a single space.
+Input three coordinate of the circle:
+9 3 6 8 3 6
+Radius of the said circle:
+3.358
+Central coordinate (x, y) of the circle:
+6.071 4.643
+
+10. Write a Python program to check whether a point (x,y) is in a triangle or not. There is a triangle formed by three points.
+Input:
+x1,y1,x2,y2,x3,y3,xp,yp separated by a single space.
+Input three coordinate of the circle:
+9 3 6 8 3 6
+Radius of the said circle:
+3.358
+Central coordinate (x, y) of the circle:
+6.071 4.643
+
+"""
