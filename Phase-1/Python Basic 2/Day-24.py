e5cfcb614ebaa924d598115d14182afb47e9134d
Mbaoma Chioma (Omarrr) <49791498+Mbaoma@users.noreply.github.com>
2020-06-06 08:05:41 +0100

    Questions 71-80

---
+++ Phase-1/Python Basic 2/Day-24.py
@@ -0,0 +1,100 @@
+"""
+
+1. Write a Python program to reverse only the vowels of a given string.
+Sample Output:
+w3resuorce
+Python
+Perl
+ASU
+
+2. Write a Python program to check whether a given integer is a palindrome or not.
+Note: An integer is a palindrome when it reads the same backward as forward. Negative numbers are not palindromic.
+Sample Output:
+False
+True
+False
+
+3. Write a Python program to remove the duplicate elements of a given array of numbers such that each element appear only once and return the new length of the given array.
+Sample Output:
+5
+4
+
+4. Write a Python program to calculate the maximum profit from selling and buying values of stock. An array of numbers represent the stock prices in chronological order.
+For example, given [8, 10, 7, 5, 7, 15], the function will return 10, since the buying value of the stock is 5 dollars and sell value is 15 dollars.
+Sample Output:
+10
+7
+0
+
+5. Write a Python program to remove all instances of a given value from a given array of integers and find the length of the new array.
+For example, given [8, 10, 7, 5, 7, 15], the function will return 10, since the buying value of the stock is 5 dollars and sell value is 15 dollars.
+Sample Output:
+6
+0
+5
+0
+
+6. Write a Python program to find the starting and ending position of a given value in a given array of integers, sorted in ascending order.
+If the target is not found in the array, return [0, 0].
+Input: [5, 7, 7, 8, 8, 8] target value = 8
+Output: [0, 5]
+Input: [1, 3, 6, 9, 13, 14] target value = 4
+Output: [0, 0]
+Sample Output:
+[0, 5]
+[0, 0]
+
+7. The price of a given stock on each day is stored in an array.
+Write a Python program to find the maximum profit in one transaction i.e., buy one and sell one share of the stock from the given price value of the said array. You cannot sell a stock before you buy one.
+Input (Stock price of each day): [224, 236, 247, 258, 259, 225]
+Output: 35
+Explanation:
+236 - 224 = 12
+247 - 224 = 23
+258 - 224 = 34
+259 - 224 = 35
+225 - 224 = 1
+247 - 236 = 11
+258 - 236 = 22
+259 - 236 = 23
+225 - 236 = -11
+258 - 247 = 11
+259 - 247 = 12
+225 - 247 = -22
+259 - 258 = 1
+225 - 258 = -33
+225 - 259 = -34
+
+8. Write a Python program to print a given N by M matrix of numbers line by line in forward > backwards > forward >... order.
+Input matrix:
+[[1, 2, 3,4],
+[5, 6, 7, 8],
+[0, 6, 2, 8],
+[2, 3, 0, 2]]
+Output:
+1
+2
+3
+4
+8
+7
+6
+5
+0
+6
+2
+8
+2
+0
+3
+2
+
+9. Write a Python program to compute the largest product of three integers from a given list of integers.
+Sample Output:
+4000
+8
+120
+
+10. Write a Python program to find the first missing positive integer that does not exist in a given list.
+
+"""
