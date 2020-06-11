9c79c0412a30f3512630f0d8bc9ac68788e8ef2d
Mbaoma Chioma (Omarrr) <49791498+Mbaoma@users.noreply.github.com>
2020-06-11 07:38:19 +0100

    Questions 91-100

---
+++ Phase-1/Python Basic 2/Day-26.py
@@ -0,0 +1,85 @@
+'''
+1. Write a Python program to count the number of arguments in a given function.
+Sample Output:
+0
+1
+2
+3
+4
+1
+
+2. Write a Python program to compute cumulative sum of numbers of a given list.
+Note: Cumulative sum = sum of itself + all previous numbers in the said list.
+Sample Output:
+[10, 30, 60, 100, 150, 210, 217]
+[1, 3, 6, 10, 15]
+[0, 1, 3, 6, 10, 15]
+
+3. Write a Python program to find the middle character(s) of a given string. If the length of the string is odd return the middle character and return the middle two characters if the string length is even.
+Sample Output:
+th
+H
+av
+
+4. Write a Python program to find the largest product of the pair of adjacent elements from a given list of integers.
+Sample Output:
+30
+20
+6
+
+5. Write a Python program to check whether every even index contains an even number and every odd index contains odd number of a given list.
+Sample Output:
+True
+False
+True
+
+6. Write a Python program to check whether a given number is a narcissistic number or not.
+If you are a reader of Greek mythology, then you are probably familiar with Narcissus. He was a hunter of exceptional beauty that he died because he was unable to leave a pool after falling in love with his own reflection. That's why I keep myself away from pools these days (kidding).
+In mathematics, he has kins by the name of narcissistic numbers - numbers that can't get enough of themselves. In particular, they are numbers that are the sum of their digits when raised to the power of the number of digits.
+For example, 371 is a narcissistic number; it has three digits, and if we cube each digits 33 + 73 + 13 the sum is 371. Other 3-digit narcissistic numbers are
+153 = 13 + 53 + 33
+370 = 33 + 73 + 03
+407 = 43 + 03 + 73.
+There are also 4-digit narcissistic numbers, some of which are 1634, 8208, 9474 since
+1634 = 14+64+34+44
+8208 = 84+24+04+84
+9474 = 94+44+74+44
+It has been proven that there are only 88 narcissistic numbers (in the decimal system) and that the largest of which is
+115,132,219,018,763,992,565,095,597,973,971,522,401
+has 39 digits.
+Ref.: //https://bit.ly/2qNYxo2
+Sample Output:
+True
+True
+True
+False
+True
+True
+True
+False
+
+7. Write a Python program to find the highest and lowest number from a given string of space separated integers.
+Sample Output:
+(77, 0)
+(0, -77)
+(0, 0)
+
+8. Write a Python program to check whether a sequence of numbers has an increasing trend or not.
+Sample Output:
+True
+False
+False
+True
+False
+
+9. Write a Python program to find the position of the second occurrence of a given string in another given string. If there is no such string return -1.
+Sample Output:
+-1
+31
+
+10. Write a Python program to compute the sum of all items of a given array of integers where each integer is multiplied by its index. Return 0 if there is no number.
+Sample Output:
+20
+-20
+0
+'''
