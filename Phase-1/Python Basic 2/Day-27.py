749df3661e49ec9fc8c31360c171987004a05a3f
Mbaoma Chioma (Omarrr) <49791498+Mbaoma@users.noreply.github.com>
2020-06-12 08:29:11 +0100

    Questions 101-110

---
+++ Phase-1/Python Basic 2/Day-27.py
@@ -0,0 +1,88 @@
+'''
+
+1. Write a Python program to find the name of the oldest student from a given dictionary containing the names and ages of a group of students.
+Sample Output:
+Nidia Dominique
+Becki Saunder
+
+2. Write a Python program to create a new string with no duplicate consecutive letters from a given string.
+Sample Output:
+PYTHON
+Python
+Java
+PHP
+
+3. Write a Python program to check whether two given lines are parallel or not.
+Note: Parallel lines are two or more lines that never intersect. Parallel Lines are like railroad tracks that never intersect.
+The General Form of the equation of a straight line is: ax + by = c
+The said straight line is represented in a list as [a, b, c]
+Example of two parallel lines:
+x + 4y = 10 and x + 4y = 14
+Sample Output:
+True
+False
+
+4. Write a Python program to create a list of the accumulating sum from a given list.
+Sample Output:
+[1, 3, 6, 10, 15]
+[-1, -3, -6, -10, -15]
+[2, 6, 12, 20, 30]
+
+5. Write a Python program to check whether a given sequence is linear, quadratic or cubic.
+Sequences are sets of numbers that are connected in some way.
+Linear sequence:
+A number pattern which increases or decreases by the same amount each time is called a linear sequence. The amount it increases or decreases by is known as the common difference.
+Quadratic sequence:
+In quadratic sequence, the difference between each term increases, or decreases, at a constant rate.
+Cubic sequence:
+Sequences where the 3rd difference are known as cubic sequence.
+Sample Output:
+Linear Sequence
+Quadratic Sequence
+Cubic Sequence
+Linear Sequence
+
+6. Write a Python program to test whether a given integer is pandigital number or not.
+From Wikipedia,
+In mathematics, a pandigital number is an integer that in a given base has among its significant digits each digit used in the base at least once.
+For example,
+1223334444555556666667777777888888889999999990 is a pandigital number in base 10.
+The first few pandigital base 10 numbers are given by:
+1023456789, 1023456798, 1023456879, 1023456897, 1023456978, 1023456987, 1023457689
+Sample Output:
+True
+True
+True
+True
+False
+
+7. Write a Python program to check whether a given number is Oddish or Evenish.
+A number is called "Oddish" if the sum of all of its digits is odd, and a number is called "Evenish" if the sum of all of its digits is even.
+Sample Output:
+Oddish
+Evenish
+Oddish
+Evenish
+Oddish
+
+8. Write a Python program that takes three integers and check whether the last digit of first number * the last digit of second number = the last digit of third number.
+Sample Output:
+True
+True
+True
+False
+False
+
+9. Write a Python program find the indices of all occurrences of a given item in a given list.
+Sample Output:
+[1, 5]
+[0, 3, 7, 8]
+[3, 6]
+
+10. Write a Python program to remove two duplicate numbers from a given number of list.
+Sample Output:
+[1, 4, 5]
+[1, 3, 4, 5]
+[1, 2, 3, 4, 5]
+
+'''
