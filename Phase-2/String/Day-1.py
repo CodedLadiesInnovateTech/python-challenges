3b3ab48631409f53a06606ced0368e71c7d0ba8b
Mbaoma Chioma (Omarrr) <49791498+Mbaoma@users.noreply.github.com>
2020-06-15 19:24:15 +0100

    Create Day-1.py

---
+++ Phase-2/String/Day-1.py
@@ -0,0 +1,44 @@
+'''
+
+1. Write a Python program to calculate the length of a string.
+
+2. Write a Python program to count the number of characters (character frequency) in a string.
+    Sample String : google.com'
+    Expected Result : {'o': 3, 'g': 2, '.': 1, 'e': 1, 'l': 1, 'm': 1, 'c': 1}
+
+3. Write a Python program to get a string made of the first 2 and the last 2 chars from a given a string. If the string length is less than 2, return instead of the empty string.
+    Sample String : 'codedladies'
+    Expected Result : 'coes'
+    Sample String : 'co'
+    Expected Result : 'coco'
+    Sample String : ' c'
+    Expected Result : Empty String
+    Click me to see the sample solution
+    
+4. Write a Python program to get a string from a given string where all occurrences of its first char have been changed to '$', except the first char itself.
+    Sample String : 'restart'
+    Expected Result : 'resta$t'
+
+5. Write a Python program to get a single string from two given strings, separated by a space and swap the first two characters of each string.
+    Sample String : 'abc', 'xyz'
+    Expected Result : 'xyc abz'
+
+6. Write a Python program to add 'ing' at the end of a given string (length should be at least 3). If the given string already ends with 'ing' then add 'ly' instead. If the string length of the given string is less than 3, leave it unchanged.
+    Sample String : 'abc'
+    Expected Result : 'abcing'
+    Sample String : 'string'
+    Expected Result : 'stringly'
+
+7. Write a Python program to find the first appearance of the substring 'not' and 'poor' from a given string, if 'not' follows the 'poor', replace the whole 'not'...'poor' substring with 'good'. Return the resulting string. 
+    Sample String : 'The lyrics is not that poor!'
+    'The lyrics is poor!'
+    Expected Result : 'The lyrics is good!'
+    'The lyrics is poor!'
+
+8. Write a Python function that takes a list of words and returns the length of the longest one. 
+
+9. Write a Python program to remove the nth index character from a nonempty string. 
+
+10. Write a Python program to change a given string to a new string where the first and last chars have been exchanged.
+
+'''
