eaa4c288fb3269d59091a17d28fc8b6150b8947c
Mbaoma Chioma (Omarrr) <49791498+Mbaoma@users.noreply.github.com>
2020-06-17 10:07:50 +0100

    questions 11-20

---
+++ Phase-2/String/Day-30.py
@@ -0,0 +1,39 @@
+'''
+
+1. Write a Python program to remove the characters which have odd index values of a given string. 
+
+2. Write a Python program to count the occurrences of each word in a given sentence. 
+
+3. Write a Python script that takes input from the user and displays that input back in upper and lower cases. 
+
+4. Write a Python program that accepts a comma separated sequence of words as input and prints the unique words in sorted form (alphanumerically). 
+Sample Words : red, white, black, red, green, black
+Expected Result : black, green, red, white,red
+
+5. Write a Python function to create the HTML string with tags around the word(s). 
+Sample function and result :
+add_tags('i', 'Python') -> '<i>Python</i>'
+add_tags('b', 'Python Tutorial') -> '<b>Python Tutorial </b>'
+
+6. Write a Python function to insert a string in the middle of a string. 
+Sample function and result :
+insert_sting_middle('[[]]<<>>', 'Python') -> [[Python]]
+insert_sting_middle('{{}}', 'PHP') -> {{PHP}}
+
+7. Write a Python function to get a string made of 4 copies of the last two characters of a specified string (length must be at least 2). 
+Sample function and result :
+insert_end('Python') -> onononon
+insert_end('Exercises') -> eseseses
+
+8. Write a Python function to get a string made of its first three characters of a specified string. If the length of the string is less than 3 then return the original string. 
+Sample function and result :
+first_three('ipy') -> ipy
+first_three('python') -> pyt
+
+9. Write a Python program to get the last part of a string before a specified character. 
+https://www.codedladiesinnovateyech.com/about-us
+Output:
+https://www.codedladiesinnovateyech.com/about
+
+10. Write a Python function to reverses a string if it's length is a multiple of 4. 
+'''
