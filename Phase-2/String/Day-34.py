153e7509c019c5f46896f2525cc37278c24fd4b8
Mbaoma Chioma (Omarrr) <49791498+Mbaoma@users.noreply.github.com>
2020-06-20 08:53:37 +0100

    questions 51-60

---
+++ Phase-2/String/Day-34.py
@@ -0,0 +1,22 @@
+'''
+
+1. Write a Python program to find the first non-repeating character in given string.
+
+2. Write a Python program to print all permutations with given repetition number of characters of a given string.
+
+3. Write a Python program to find the first repeated character in a given string.
+
+4. Write a Python program to find the first repeated character of a given string where the index of first occurrence is smallest.
+
+5.Write a Python program to find the first repeated word in a given string.
+
+6. Write a Python program to find the second most repeated word in a given string.
+
+7.Write a Python program to remove spaces from a given string.
+
+8. Write a Python program to move spaces to the front of a given string.
+
+9. Write a Python program to find the maximum occurring character in a given string.
+
+10. Write a Python program to capitalize first and last letters of each word of a given string.
+'''
