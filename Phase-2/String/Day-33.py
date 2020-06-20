6fe189fdff78077d7b8e5ffb6dc1adc51dbd30d2
Winner Emeto <emetowinner@gmail.com>
2020-06-20 08:24:59 +0100

    Create Day-33.py

---
+++ Phase-2/String/Day-33.py
@@ -0,0 +1,63 @@
+"""
+1. Write a Python program to strip a set of characters from a string. 
+
+2. Write a Python program to count repeated characters in a string. 
+
+Sample string: 'thequickbrownfoxjumpsoverthelazydog'
+
+Expected output :
+
+o 4
+
+e 3
+
+u 2
+
+h 2
+
+r 2
+
+t 2
+
+3. Write a Python program to print the square and cube symbol in the area of a rectangle and volume of a cylinder. 
+
+Sample output:
+
+The area of the rectangle is 1256.66cm2
+
+The volume of the cylinder is 1254.725cm3
+
+4. Write a Python program to print the index of the character in a string. 
+
+Sample string: w3resource
+
+Expected output:
+
+Current character w position at 0
+
+Current character 3 position at 1
+
+Current character r position at 2
+
+- - - - - - - - - - - - - - - - - - - - - - - - -
+
+Current character c position at 8
+
+Current character e position at 9
+
+5. Write a Python program to check if a string contains all letters of the alp
+
+6. Write a Python program to convert a string in a list.
+
+7. Write a Python program to lowercase first n characters in a string.
+
+8. Write a Python program to swap comma and dot in a string.
+
+Sample string: "32.054,23"
+
+Expected Output: "32,054.23"
+
+9. Write a Python program to count and display the vowels of a given text.
+
+10. Write a Python program to split a string on the last occurrence of the delimiter.
+"""
