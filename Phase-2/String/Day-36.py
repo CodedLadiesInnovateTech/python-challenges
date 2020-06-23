fa9cd751023de4ff693375b8e8d9abe6ea04fdca
Mbaoma Chioma (Omarrr) <49791498+Mbaoma@users.noreply.github.com>
2020-06-23 13:44:45 +0100

    questions 71-80

---
+++ Phase-2/String/Day-36.py
@@ -0,0 +1,25 @@
+'''
+1. Write a Python program to move all spaces to the front of a given string in single traversal.
+
+2. Write a Python program to remove all consecutive duplicates from a given string.
+
+3. Write a Python program to count Uppercase, Lowercase, special character and numeric values in a given string.
+
+4. Write a Python program to find the minimum window in a given string which will contain all the characters of another given string.
+Example 1
+Input : str1 = " PRWSOERIUSFK "
+str2 = " OSU "
+Output: Minimum window is "OERIUS"
+
+5. Write a Python program to find smallest window that contains all characters of a given string.
+
+6. Write a Python program to count number of substrings from a given string of lowercase alphabets with exactly k distinct (given) characters.
+
+7. Write a Python program to count number of non-empty substrings of a given string.
+
+8. Write a Python program to count characters at same position in a given string (lower and uppercase characters) as in English alphabet.
+
+9. Write a Python program to find smallest and largest word in a given string.
+
+10. Write a Python program to count number of substrings with same first and last characters of a given string.
+'''
