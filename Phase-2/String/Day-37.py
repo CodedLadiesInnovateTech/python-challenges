be86e51ff52705ec5928879200cc28c28c218fac
Mbaoma Chioma (Omarrr) <49791498+Mbaoma@users.noreply.github.com>
2020-06-24 19:13:00 +0100

    Questions 81-84

---
+++ Phase-2/String/Day-37.py
@@ -0,0 +1,25 @@
+'''
+
+1. Write a Python program to find the index of a given string at which a given substring starts. If the substring is not found in the given string return 'Not found'. 
+
+2. Write a Python program to wrap a given string into a paragraph of given width.
+    Sample Output:
+    Input a string: The quick brown fox.
+    Input the width of the paragraph: 10
+    Result:
+    The quick
+    brown fox.
+
+3. Write a Python program to print four values decimal, octal, hexadecimal (capitalized), binary in a single line of a given integer.
+    Sample Output:
+    Input an integer: 25
+    Decimal Octal Hexadecimal (capitalized), Binary
+    25 31 19 11001
+
+4. Write a Python program to swap cases of a given string.
+    Sample Output:
+    pYTHON eXERCISES
+    jAVA
+    nUMpY
+
+'''
