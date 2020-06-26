35e7a291c252471a45d527f971dbca14c3b95369
emetowinner <emetowinner@gmaile.com>
2020-06-26 03:46:52 +0100

    updated task

---
+++ Phase-2/Json/Day-38.py
@@ -0,0 +1,25 @@
+1. Write a Python program to convert JSON data to Python object.
+
+
+2. Write a Python program to convert Python object to JSON data.
+
+
+3. Write a Python program to convert Python objects into JSON strings. Print all the values.
+
+
+4. Write a Python program to convert Python dictionary object (sort by key) to JSON data. Print the object members with indent level 4.
+
+
+5. Write a Python program to convert JSON encoded data into Python objects.
+
+
+6. Write a Python program to create a new JSON file from an existing JSON file.
+
+
+7. Write a Python program to check whether an instance is complex or not.
+
+
+8. Write a Python program to check whether a JSON string contains complex object or not.
+
+
+9. Write a Python program to access only unique key value of a Python object.
