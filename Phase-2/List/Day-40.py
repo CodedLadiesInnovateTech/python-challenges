84ae4533cf06c2d2a869ed447906995bf3da8977
Mbaoma Chioma (Omarrr) <49791498+Mbaoma@users.noreply.github.com>
2020-06-27 21:35:11 +0100

    questions 11-20

---
+++ Phase-2/List/Day-40.py
@@ -0,0 +1,24 @@
+'''
+1. Write a Python function that takes two lists and returns True if they have at least one common member. 
+
+2. Write a Python program to print a specified list after removing the 0th, 4th and 5th elements. 
+Sample List : ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+Expected Output : ['Green', 'White', 'Black']
+
+3. Write a Python program to generate a 3*4*6 3D array whose each element is *. 
+
+4. Write a Python program to print the numbers of a specified list after removing even numbers from it. 
+
+5. Write a Python program to shuffle and print a specified list. 
+
+6. Write a Python program to generate and print a list of first and last 5 elements where the values are square of numbers between 1 and 30 (both included). 
+
+7. Write a Python program to generate and print a list except for the first 5 elements, where the values are square of numbers between 1 and 30 (both included). 
+
+8. Write a Python program to generate all permutations of a list in Python. 
+
+9. Write a Python program to get the difference between the two lists. 
+
+10. Write a Python program access the index of a list. 
+
+'''
