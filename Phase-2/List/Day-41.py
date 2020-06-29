d6b5ccd93860f8fb5c787fe13de1adccd8bc0641
Mbaoma Chioma (Omarrr) <49791498+Mbaoma@users.noreply.github.com>
2020-06-29 23:28:09 +0100

    questions 21-30

---
+++ Phase-2/List/Day-41.py
@@ -0,0 +1,21 @@
+'''
+1. Write a Python program to convert a list of characters into a string. 
+
+2. Write a Python program to find the index of an item in a specified list. 
+
+3. Write a Python program to flatten a shallow list. 
+
+4. Write a Python program to append a list to the second list. 
+
+5. Write a Python program to select an item randomly from a list. 
+
+6. Write a python program to check whether two lists are circularly identical. 
+
+7. Write a Python program to find the second smallest number in a list. 
+
+8. Write a Python program to find the second largest number in a list. 
+
+9. Write a Python program to get unique values from a list. 
+
+10. Write a Python program to get the frequency of the elements in a list. 
+'''
