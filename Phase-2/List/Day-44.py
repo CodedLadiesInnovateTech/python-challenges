ae53d7a83d56cb0f273bab58efa9675e5b478cf5
Mbaoma Chioma (Omarrr) <49791498+Mbaoma@users.noreply.github.com>
2020-07-04 08:35:29 +0100

    Questions 51-60

---
+++ Phase-2/List/Day-44.py
@@ -0,0 +1,30 @@
+'''
+
+1. Write a Python program to split a list every Nth element. 
+Sample list: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n']
+Expected Output: [['a', 'd', 'g', 'j', 'm'], ['b', 'e', 'h', 'k', 'n'], ['c', 'f', 'i', 'l']]
+
+2. Write a Python program to compute the similarity between two lists. 
+Sample data: ["red", "orange", "green", "blue", "white"], ["black", "yellow", "green", "blue"]
+Expected Output:
+Color1-Color2: ['white', 'orange', 'red']
+Color2-Color1: ['black', 'yellow']
+
+3. Write a Python program to create a list with infinite elements. 
+
+4. Write a Python program to concatenate elements of a list. 
+
+5. Write a Python program to remove key values pairs from a list of dictionaries. 
+
+6. Write a Python program to convert a string to a list. 
+
+7. Write a Python program to check whether all items of a list is equal to a given string. 
+
+8. Write a Python program to replace the last element in a list with another list. 
+Sample data : [1, 3, 5, 7, 9, 10], [2, 4, 6, 8]
+Expected Output: [1, 3, 5, 7, 9, 2, 4, 6, 8]
+
+9. Write a Python program to check whether the n-th element exists in a given list. 
+
+10. Write a Python program to find a tuple, the smallest second index value from a list of tuples
+'''
