66e7a7b247ccab5c63c17ee0668478af24b836d2
Mbaoma Chioma (Omarrr) <49791498+Mbaoma@users.noreply.github.com>
2020-07-08 19:34:06 +0100

    questions 81-82

---
+++ Phase-2/List/Day-47.py
@@ -0,0 +1,11 @@
+'''
+1. Write a Python program to extract a given number of randomly selected elements from a given list. 
+Original list:
+[1, 1, 2, 3, 4, 4, 5, 1]
+Selected 3 random numbers of the above list:
+[4, 4, 1]
+
+2. Write a Python program to generate the combinations of n distinct objects taken from the elements of a given list. 
+Original list: [1, 2, 3, 4, 5, 6, 7, 8, 9] Combinations of 2 distinct objects: [1, 2] [1, 3] [1, 4] [1, 5] .... [7, 8] [7, 9] [8, 9]
+
+'''
