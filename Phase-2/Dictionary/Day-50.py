c5868bbbca070741d3bcd5a285eebbef2b06be70
Mbaoma Chioma (Omarrr) <49791498+Mbaoma@users.noreply.github.com>
2020-07-13 09:29:21 +0100

    Questions 21-30

---
+++ Phase-2/Dictionary/Day-50.py
@@ -0,0 +1,41 @@
+'''
+
+1. Write a Python program to create and display all combinations of letters, selecting each letter from a different key in a dictionary.
+Sample data : {'1':['a','b'], '2':['c','d']}
+Expected Output:
+ac
+ad
+bc
+bd
+
+2. Write a Python program to find the highest 3 values in a dictionary.
+
+3. Write a Python program to combine values in python list of dictionaries.
+Sample data: [{'item': 'item1', 'amount': 400}, {'item': 'item2', 'amount': 300}, {'item': 'item1', 'amount': 750}]
+Expected Output: Counter({'item1': 1150, 'item2': 300})
+
+4. Write a Python program to create a dictionary from a string.
+Note: Track the count of the letters from the string.
+Sample string : 'w3resource'
+Expected output: {'3': 1, 's': 1, 'r': 2, 'u': 1, 'w': 1, 'c': 1, 'e': 2, 'o': 1}
+
+5. Write a Python program to print a dictionary in table format.
+ 
+6. Write a Python program to count the values associated with key in a dictionary.
+Sample data: = [{'id': 1, 'success': True, 'name': 'Lary'}, {'id': 2, 'success': False, 'name': 'Rabi'}, {'id': 3, 'success': True, 'name': 'Alex'}]
+Expected result: Count of how many dictionaries have success as True
+
+7. Write a Python program to convert a list into a nested dictionary of keys.
+
+8. Write a Python program to sort a list alphabetically in a dictionary.
+
+9. Write a Python program to remove spaces from dictionary keys.
+
+10. Write a Python program to get the top three items in a shop.
+Sample data: {'item1': 45.50, 'item2':35, 'item3': 41.30, 'item4':55, 'item5': 24}
+Expected Output:
+item4 55
+item1 45.5
+item3 41.3
+
+'''
