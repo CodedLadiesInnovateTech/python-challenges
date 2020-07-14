9ac41c49e310538ac55ccc59ff4c7fefc8415219
Mbaoma Chioma (Omarrr) <49791498+Mbaoma@users.noreply.github.com>
2020-07-14 20:59:18 +0100

    Questions 31-40

---
+++ Phase-2/Dictionary/Day-51.py
@@ -0,0 +1,42 @@
+'''
+1. Write a Python program to get the key, value and item in a dictionary.
+
+2. Write a Python program to print a dictionary line by line.
+
+3. Write a Python program to check multiple keys exists in a dictionary.
+
+4. Write a Python program to count number of items in a dictionary value that is a list.
+
+5. Write a Python program to sort Counter by value.
+Sample data : {'Math':81, 'Physics':83, 'Chemistry':87}
+Expected data: [('Chemistry', 87), ('Physics', 83), ('Math', 81)]
+
+6. Write a Python program to create a dictionary from two lists without losing duplicate values.
+Sample lists: ['Class-V', 'Class-VI', 'Class-VII', 'Class-VIII'], [1, 2, 2, 3]
+Expected Output: defaultdict(<class 'set'>, {'Class-VII': {2}, 'Class-VI': {2}, 'Class-VIII': {3}, 'Class-V': {1}})
+
+7. Write a Python program to replace dictionary values with their sum.
+
+8. Write a Python program to match key values in two dictionaries.
+Sample dictionary: {'key1': 1, 'key2': 3, 'key3': 2}, {'key1': 1, 'key2': 2}
+Expected output: key1: 1 is present in both x and y
+
+9. Write a Python program to store a given dictionary in a json file.
+Original dictionary:
+{'students': [{'firstName': 'Nikki', 'lastName': 'Roysden'}, {'firstName': 'Mervin', 'lastName': 'Friedland'}, {'firstName': 'Aron ', 'lastName': 'Wilkins'}], 'teachers': [{'firstName': 'Amberly', 'lastName': 'Calico'}, {'firstName': 'Regine', 'lastName': 'Agtarap'}]}
+<class 'dict'>
+Json file to dictionary:
+{'students': [{'firstName': 'Nikki', 'lastName': 'Roysden'}, {'firstName': 'Mervin', 'lastName': 'Friedland'}, {'firstName': 'Aron ', 'lastName': 'Wilkins'}], 'teachers': [{'firstName': 'Amberly', 'lastName': 'Calico'}, {'firstName': 'Regine', 'lastName': 'Agtarap'}]}
+
+10. Write a Python program to create a dictionary of keys x, y, and z where each key has as value a list from 11-20, 21-30, and 31-40 respectively. Access the fifth value of each key from the dictionary.
+{'x': [11, 12, 13, 14, 15, 16, 17, 18, 19],
+'y': [21, 22, 23, 24, 25, 26, 27, 28, 29],
+'z': [31, 32, 33, 34, 35, 36, 37, 38, 39]}
+15
+25
+35
+x has value [11, 12, 13, 14, 15, 16, 17, 18, 19]
+y has value [21, 22, 23, 24, 25, 26, 27, 28, 29]
+z has value [31, 32, 33, 34, 35, 36, 37, 38, 39]
+
+'''
