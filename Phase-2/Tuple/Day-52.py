d786b219d86cd1729b1935b7a62281d1ae35b5fb
Mbaoma Chioma (Omarrr) <49791498+Mbaoma@users.noreply.github.com>
2020-07-16 12:13:11 +0100

    questions 1-10

---
+++ Phase-2/Tuple/Day-52.py
@@ -0,0 +1,21 @@
+'''
+1. Write a Python program to create a tuple.
+
+2. Write a Python program to create a tuple with different data types.
+
+3. Write a Python program to create a tuple with numbers and print one item.
+
+4. Write a Python program to unpack a tuple in several variables.
+
+5. Write a Python program to add an item in a tuple.
+
+6. Write a Python program to convert a tuple to a string.
+
+7. Write a Python program to get the 4th element and 4th element from last of a tuple.
+
+8. Write a Python program to create the colon of a tuple.
+
+9. Write a Python program to find the repeated items of a tuple.
+
+10. Write a Python program to check whether an element exists within a tuple.
+'''
