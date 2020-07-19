21bea8c88c91200393ccf3d9a8c428d119ae11be
Mbaoma Chioma (Omarrr) <49791498+Mbaoma@users.noreply.github.com>
2020-07-19 13:45:54 +0100

    questions 21-24

---
+++ Phase-2/Tuple/Day-54.py
@@ -0,0 +1,15 @@
+'''
+1. Write a Python program to replace last value of tuples in a list.
+Sample list: [(10, 20, 40), (40, 50, 60), (70, 80, 90)]
+Expected Output: [(10, 20, 100), (40, 50, 100), (70, 80, 100)]
+
+2. Write a Python program to remove an empty tuple(s) from a list of tuples.
+Sample data: [(), (), ('',), ('a', 'b'), ('a', 'b', 'c'), ('d')]
+Expected output: [('',), ('a', 'b'), ('a', 'b', 'c'), 'd']
+
+3. Write a Python program to sort a tuple by its float element.
+Sample data: [('item1', '12.20'), ('item2', '15.10'), ('item3', '24.5')]
+Expected Output: [('item3', '24.5'), ('item2', '15.10'), ('item1', '12.20')]
+
+4. Write a Python program to count the elements in a list until an element is a tuple.
+'''
