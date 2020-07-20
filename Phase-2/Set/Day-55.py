4488f240d36fe0c7502b96fc2da7439aeb441ac1
Mbaoma Chioma (Omarrr) <49791498+Mbaoma@users.noreply.github.com>
2020-07-20 21:51:02 +0100

    questions 1-10

---
+++ Phase-2/Set/Day-55.py
@@ -0,0 +1,21 @@
+'''
+1. Write a Python program to create a set.
+
+2. Write a Python program to iterate over sets.
+
+3. Write a Python program to add member(s) in a set.
+
+4. Write a Python program to remove item(s) from set
+
+5. Write a Python program to remove an item from a set if it is present in the set.
+
+6. Write a Python program to create an intersection of sets.
+
+7. Write a Python program to create a union of sets.
+
+8. Write a Python program to create set difference.
+
+9. Write a Python program to create a symmetric difference.
+
+10. Write a Python program to check if a set is a subset of another set.
+'''
