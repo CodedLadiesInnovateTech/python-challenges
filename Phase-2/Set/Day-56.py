9b433cde1475b1028acedf8cb3e5a852e047623f
Mbaoma Chioma (Omarrr) <49791498+Mbaoma@users.noreply.github.com>
2020-07-22 07:49:36 +0100

    Questions 11-20

---
+++ Phase-2/Set/Day-56.py
@@ -0,0 +1,25 @@
+'''
+
+1. Write a Python program to create a shallow copy of sets.
+Note : Shallow copy is a bit-wise copy of an object. A new object is created that has an exact copy of the values in the original object.
+
+2. Write a Python program to clear a set.
+
+3. Write a Python program to use of frozensets.
+Note: Frozensets behave just like sets except they are immutable.
+
+4. Write a Python program to find maximum and the minimum value in a set.
+
+5. Write a Python program to find the length of a set.
+
+6. Write a Python program to check if a given value is present in a set or not.
+
+7. Write a Python program to check if two given sets have no elements in common.
+
+8. Write a Python program to check if a given set is superset of itself and superset of another given set.
+
+9. Write a Python program to find the elements in a given set that are not in another set.
+
+10. Write a Python program to check a given set has no elements in common with other given set.
+
+'''
