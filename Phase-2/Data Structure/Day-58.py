55f3faece37e0a79e838a3b27d975ec959f812cd
Mbaoma Chioma (Omarrr) <mmaryraphaella@gmail.com>
2020-07-23 23:17:15 +0100

    questions 1-10

---
+++ Phase-2/Data Structure/Day-58.py
@@ -0,0 +1,83 @@
+'''
+1. Write a Python program to create an Enum object and display a member name and value.
+Sample data :
+Afghanistan = 93
+Albania = 355
+Algeria = 213
+Andorra = 376
+Angola = 244
+Antarctica = 672
+Expected Output :
+Member name: Albania
+Member value: 355
+
+2. Write a Python program to iterate over an enum class and display individual member and their value.
+Expected Output:
+Afghanistan = 93
+Albania = 355
+Algeria = 213
+Andorra = 376
+Angola = 244
+Antarctica = 672
+
+3. Write a Python program to display all the member name of an enum class ordered by their values.
+Expected Output:
+Country Name ordered by Country Code:
+Afghanistan
+Algeria
+Angola
+Albania
+Andorra
+Antarctica
+
+4. Write a Python program to get all values from an enum class.
+Expected output:
+[93, 355, 213, 376, 244, 672]
+
+5. Write a Python program to count the most common words in a dictionary.
+Expected Output:
+[('pink', 6), ('black', 5), ('white', 5), ('red', 4)]
+
+6. Write a Python program to find the class wise roll number from a tuple-of-tuples.
+Expected Output:
+defaultdict(, {'VII': [1], 'V': [1, 2], 'VI': [1, 2, 3]})
+
+7. Write a Python program to count the number of students of individual class.
+Sample data:
+classes = (
+('V', 1),
+('VI', 1),
+('V', 2),
+('VI', 2),
+('VI', 3),
+('VII', 1),
+)
+Expected Output:
+Counter({'VI': 3, 'V': 2, 'VII': 1})
+
+8.Write a Python program to get the unique enumeration values.
+Expected Output:
+Afghanistan = 93
+Albania = 355
+Algeria = 213
+Andorra = 376
+Angola = 244
+
+9. Write a Python program to create an instance of an OrderedDict using a given dictionary. Sort the dictionary during the creation and print the members of the dictionary in reverse order.
+Expected Output:
+Angola 244
+Andorra 376
+Algeria 213
+Afghanistan 93
+Albania 355
+In reverse order:
+Albania 355
+Afghanistan 93
+Algeria 213
+Andorra 376
+Angola 244
+
+10. Write a Python program to group a sequence of key-value pairs into a dictionary of lists.
+Expected output:
+[('v', [1, 3]), ('vi', [2, 4]), ('vii', [1])]
+'''
