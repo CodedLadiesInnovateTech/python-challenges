f95501516489b2d9931d11c0ef20e17e1c248bc7
Mbaoma Chioma (Omarrr) <mmaryraphaella@gmail.com>
2020-07-25 09:46:17 +0100

    Questions 11-20

---
+++ Phase-2/Data Structure/Day-59.py
@@ -0,0 +1,66 @@
+'''
+
+1. Write a Python program to compare two unordered lists (not sets).
+Expected Output: False
+
+2. Write a Python program to create an array contains six integers. Also print all the members of the array.
+Expected Output:
+10
+20
+30
+40
+50
+
+3. Write a Python program to get the array size of types unsigned integer and float.
+Expected Output:
+4
+4
+
+4. Write a Python program to get an array buffer information.
+Expected Output:
+Array buffer start address in memory and number of elements.
+(25855056, 2)
+
+5. Write a Python program to get the length of an array.
+Expected Output:
+Length of the array is:
+5
+
+6. Write a Python program to convert an array to an ordinary list with the same items.
+Expected Output:
+Original array:
+array('b', [1, 2, 3, 4])
+Array to list:
+[1, 2, 3, 4]
+
+7. Write a Python program to convert an array to an array of machine values and return the bytes representation.
+Expected Output:
+Original array:
+A1: array('i', [1, 2, 3, 4, 5, 6])
+Array of bytes: b'010000000200000003000000040000000500000006000000'
+
+8. Write a Python program to read a string and interpreting the string as an array of machine values.
+Expected Output:
+array1: array('i', [7, 8, 9, 10])
+Bytes: b'0700000008000000090000000a000000'
+array2: array('i', [7, 8, 9, 10])
+
+9. Write a Python program to push three items into the heap and print the items from the heap.
+Expected Output:
+('V', 1)
+('V', 2)
+('V', 3)
+
+10. Write a Python program to push three items into a heap and return the smallest item from the heap. Also Pop and return the smallest item from the heap.
+Expected Output:
+Items in the heap:
+('V', 1)
+('V', 3)
+('V', 2)
+----------------------
+The smallest item in the heap:
+('V', 1)
+----------------------
+Pop the smallest item in the heap:
+('V', 2)
+('V', 3)
