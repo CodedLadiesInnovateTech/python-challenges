eadb5d8d7dd26e1f424e3569d08cdf6fefe8be21
Mbaoma Chioma (Omarrr) <mmaryraphaella@gmail.com>
2020-07-27 09:29:48 +0100

    Questions 21-30

---
+++ Phase-2/Data Structure/Day-60.py
@@ -0,0 +1,62 @@
+'''
+
+1. Write a Python program to push an item on the heap, then pop and return the smallest item from the heap.
+Expected Output:
+Items in the heap:
+('V', 1)
+('V', 3)
+('V', 2)
+----------------------
+Using heappushpop push item on the heap and return the smallest item.
+('V', 2)
+('V', 3)
+('V', 6)
+
+2. Write a Python program to create a heapsort, pushing all values onto a heap and then popping off the smallest values one at a time.
+Expected Output:
+[10, 20, 20, 40, 50, 50, 60, 70, 80, 90, 100]
+
+3. Write a Python program to get the two largest and three smallest items from a dataset.
+Expected Output:
+[100, 90]
+[10, 20, 20]
+
+4. Write a Python program to locate the left insertion point for a specified value in sorted order.
+Expected Output:
+4
+2
+
+5. Write a Python program to locate the right insertion point for a specified value in sorted order.
+Expected Output:
+3
+2
+
+6. Write a Python program to insert items into a list in sorted order.
+Expected Output:
+Original List:
+[25, 45, 36, 47, 69, 48, 68, 78, 14, 36]
+Sorted List:
+[14, 25, 36, 36, 45, 47, 48, 68, 69, 78]
+
+7. a Python program to create a queue and display all the members and size of the queue.
+Expected Output:
+Members of the queue:
+0 1 2 3
+Size of the queue:
+4
+
+8. Write a Python program to find whether a queue is empty or not.
+Expected Output:
+True
+False
+
+9. Write a Python program to create a FIFO queue.
+Expected Output:
+0 1 2 3
+
+10. Write a Python program to create a LIFO queue.
+Expected Output:
+3 2 1 0
+
+
+'''
