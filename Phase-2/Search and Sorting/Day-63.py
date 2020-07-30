361938c8ac921d29098fe8aa1ae7e22b36993046
Mbaoma Chioma (Omarrr) <mmaryraphaella@gmail.com>
2020-07-30 16:09:21 +0100

    questions 4-6

---
+++ Phase-2/Search and Sorting/Day-63.py
@@ -0,0 +1,18 @@
+'''
+
+1. Write a Python program to sort a list of elements using the bubble sort algorithm.
+Note : According to Wikipedia "Bubble sort, sometimes referred to as sinking sort, is a simple sorting algorithm that repeatedly steps through the list to be sorted, compares each pair of adjacent items and swaps them if they are in the wrong order. The pass through the list is repeated until no swaps are needed, which indicates that the list is sorted. The algorithm, which is a comparison sort, is named for the way smaller elements "bubble" to the top of the list. Although the algorithm is simple, it is too slow and impractical for most problems even when compared to insertion sort. It can be practical if the input is usually in sort order but may occasionally have some out-of-order elements nearly in position."
+Sample Data: [14,46,43,27,57,41,45,21,70]
+Expected Result: [14, 21, 27, 41, 43, 45, 46, 57, 70]
+
+2. Write a Python program to sort a list of elements using the selection sort algorithm.
+Note : The selection sort improves on the bubble sort by making only one exchange for every pass through the list. 
+Sample Data: [14,46,43,27,57,41,45,21,70]
+Expected Result: [14, 21, 27, 41, 43, 45, 46, 57, 70]
+
+3. Write a Python program to sort a list of elements using the insertion sort algorithm.
+Note : According to Wikipedia "Insertion sort is a simple sorting algorithm that builds the final sorted array (or list) one item at a time. It is much less efficient on large lists than more advanced algorithms such as quicksort, heapsort, or merge sort."
+Sample Data: [14,46,43,27,57,41,45,21,70]
+Expected Result : [14, 21, 27, 41, 43, 45, 46, 57, 70]
+
+'''
