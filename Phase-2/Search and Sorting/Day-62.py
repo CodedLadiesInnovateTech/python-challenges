54c7f951dd863599aaa8bd88638dce446493516a
Mbaoma Chioma (Omarrr) <mmaryraphaella@gmail.com>
2020-07-30 09:17:47 +0100

    Questions 1-3

---
+++ Phase-2/Search and Sorting/Day-62.py
@@ -0,0 +1,18 @@
+'''
+
+1. Write a Python program for binary search.
+Binary Search : In computer science, a binary search or half-interval search algorithm finds the position of a target value within a sorted array. The binary search algorithm can be classified as a dichotomies divide-and-conquer search algorithm and executes in logarithmic time.
+Test Data :
+binary_search([1,2,3,5,8], 6) -> False
+binary_search([1,2,3,5,8], 5) -> True
+
+2. Write a Python program for sequential search.
+Sequential Search : In computer science, linear search or sequential search is a method for finding a particular value in a list that checks each element in sequence until the desired element is found or the list is exhausted. The list need not be ordered.
+Test Data :
+Sequential_Search([11,23,58,31,56,77,43,12,65,19],31) -> (True, 3)
+
+3. Write a Python program for binary search for an ordered list.
+Test Data :
+Ordered_binary_Search([0, 1, 3, 8, 14, 18, 19, 34, 52], 3) -> True
+Ordered_binary_Search([0, 1, 3, 8, 14, 18, 19, 34, 52], 17) -> False
+'''
