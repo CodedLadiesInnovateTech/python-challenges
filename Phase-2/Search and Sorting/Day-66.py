3eb1110f6c537043e07b137c034e405e779ebda3
Mbaoma Chioma (Omarrr) <mmaryraphaella@gmail.com>
2020-08-06 16:02:50 +0100

    Questions 16-18

---
+++ Phase-2/Search and Sorting/Day-66.py
@@ -0,0 +1,22 @@
+'''
+1. Write a Python program to sort a list of elements using Cycle sort.
+Cycle sort is an in-place, unstable sorting algorithm, a comparison sort that is theoretically optimal in terms of the total number of writes to the original array, 
+unlike any other in-place sorting algorithm. It is based on the idea that the permutation to be sorted can be factored into cycles, which can individually be rotated 
+to give a sorted result.
+
+2. Write a Python program to sort a list of elements using Heap sort.
+In computer science, heapsort (invented by J. W. J. Williams in 1964) is a comparison-based sorting algorithm.
+Heapsort can be thought of as an improved selection sort: like that algorithm, it divides its input into a sorted and an unsorted region,
+and it interactively shrinks the unsorted region by extracting the largest element and moving that to the sorted region.
+The improvement consists of the use of a heap data structure rather than a linear-time search to find the maximum.
+Although somewhat slower in practice on most machines than a well-implemented quicksort, it has the advantage of a more favorable worst-case O(n log n) runtime. 
+Heapsort is an in-place algorithm, but it is not a stable sort. A run of the heapsort algorithm sorting an array of randomly permuted values.
+In the first stage of the algorithm the array elements are reordered to satisfy the heap property. Before the actual sorting takes place, 
+the heap tree structure is shown briefly for illustration.
+
+3. Write a Python program to sort a list of elements using Pancake sort.
+Pancake sorting is the colloquial term for the mathematical problem of sorting a disordered stack of pancakes in order of size when a spatula can be inserted
+at any point in the stack and used to flip all pancakes above it. A pancake number is the minimum number of flips required for a given number of pancakes. 
+The problem was first discussed by American geometer Jacob E. Goodman. It is a variation of the sorting problem in which the only allowed operation is to
+reverse the elements of some prefix of the sequence.
+'''
