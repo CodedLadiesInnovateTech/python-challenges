c43e86a39ba6b0e00eb9660a906f0c333fd92927
Mbaoma Chioma (Omarrr) <mmaryraphaella@gmail.com>
2020-08-07 22:18:47 +0100

    Questions 19-21

---
+++ Phase-2/Search and Sorting/Day-67.py
@@ -0,0 +1,10 @@
+'''
+
+1. Write a Python program to sort a list of elements using Radix sort.
+According to Wikipedia "In computer science, radix sort is a non-comparative integer sorting algorithm that sorts data with integer keys by grouping keys by the individual digits which share the same significant position and value".
+
+2. Write a Python program to sort a list of elements using Selection sort.
+According to Wikipedia "In computer science, selection sort is a sorting algorithm, specifically an in-place comparison sort. It has O(n2) time complexity, making it inefficient on large lists, and generally performs worse than the similar insertion sort".
+
+3. Write a Python program to sort a list of elements using Time sort
+'''
