1539d8a893aea8531bb8190d3f6b3b0a8192e699
Mbaoma Chioma (Omarrr) <mmaryraphaella@gmail.com>
2020-08-13 05:40:26 +0100

    Questions 4-6

---
+++ Phase-2/Linked List/Day-70.py
@@ -0,0 +1,8 @@
+'''
+1. Write a Python program to access a specific item in a singly linked list using index value.
+
+2. Write a Python program to set a new value of an item in a singly linked list using index value.
+
+3. Write a Python program to delete the first item from a singly linked list.
+
+'''
