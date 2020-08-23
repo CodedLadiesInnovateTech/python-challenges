9bfac48869a38f6660673f5e66f7f6f503ab0818
Mbaoma Chioma (Omarrr) <mmaryraphaella@gmail.com>
2020-08-23 21:12:40 +0100

    Questions 1-3

---
+++ Phase-2/File I/O/Day-78.py
@@ -0,0 +1,5 @@
+'''
+1. Write a Python program to read an entire text file.
+2. Write a Python program to read first n lines of a file.
+3. Write a Python program to append text to a file and display the text
+'''
