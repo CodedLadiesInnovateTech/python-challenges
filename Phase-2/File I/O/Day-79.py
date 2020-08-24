81d05d8c4337adcf59c58d7e22d71e1a35df982f
Mbaoma Chioma (Omarrr) <mmaryraphaella@gmail.com>
2020-08-24 19:04:22 +0100

    Questions 4-6

---
+++ Phase-2/File I/O/Day-79.py
@@ -0,0 +1,11 @@
+'''
+
+1. Write a Python program to read last n lines of a file.
+
+2. Write a Python program to read a file line by line and 
+store it into a list.
+
+3. Write a Python program to read a file line by line store
+it into a variable
+
+'''
