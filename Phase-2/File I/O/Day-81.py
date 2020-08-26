57bc610193250d281b23dbd089a7ddc238b47ef8
Mbaoma Chioma (Omarrr) <mmaryraphaella@gmail.com>
2020-08-26 21:01:42 +0100

    questions 10-12

---
+++ Phase-2/File I/O/Day-81.py
@@ -0,0 +1,7 @@
+'''
+1. Write a Python program to count the frequency of words in a file.
+
+2. Write a Python program to get the file size of a plain file.
+
+3. Write a Python program to write a list to a file.
+'''
