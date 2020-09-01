ce5a9f6f4ea8993865800834378666c81f938faa
Mbaoma Chioma (Omarrr) <mmaryraphaella@gmail.com>
2020-09-01 21:24:36 +0100

    questions 1-3

---
+++ Phase-3/Python Challenge/Day-85.py
@@ -0,0 +1,15 @@
+'''
+
+1. Write a Python program to check if a given positive integer is a power of two.
+Input : 4
+Output : True
+
+2. Write a Python program to check if a given positive integer is a power of three.
+Input : 9
+Output : True
+
+3. Write a Python program to check if a given positive integer is a power of four.
+Input : 4
+Output : True
+
+'''
