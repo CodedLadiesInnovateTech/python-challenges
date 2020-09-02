3831469b8004c19ce8926d060ea76aedc505d8a7
Mbaoma Chioma (Omarrr) <mmaryraphaella@gmail.com>
2020-09-02 20:08:16 +0100

    questions 4-6

---
+++ Phase-3/Python Challenge/Day-86.py
@@ -0,0 +1,13 @@
+'''
+1. Write a Python program to check if a number is a perfect square.
+Input : 9
+Output : True
+
+2. Write a Python program to check if an integer is the power of another integer
+Input : 16, 2
+Output : True
+
+3. Write a Python program to check if a number is a power of a given base
+Input : 128,2
+Output : True
+'''
