ccdbeda405fe61980de43838ab56904e167dcc9e
Mbaoma Chioma (Omarrr) <mmaryraphaella@gmail.com>
2020-09-03 19:22:28 +0100

    questions 7-9

---
+++ Phase-3/Python Challenge/Day-87.py
@@ -0,0 +1,14 @@
+'''
+1. Write a Python program to find a missing number from a list.
+Input : [1,2,3,4,6,7,8]
+Output : 5
+
+2. Write a Python program to find missing numbers from a list.
+Input : [1,2,3,4,6,7,10]
+Output : [5, 8, 9]
+
+3. Write a Python program to find three numbers from an array such that the sum of three numbers equal to zero.
+Input : [-1,0,1,2,-1,-4]
+Output : [[-1, -1, 2], [-1, 0, 1]]
+Note : Find the unique triplets in the array.
+'''
