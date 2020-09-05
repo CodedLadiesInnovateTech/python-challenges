65f16b450b9515b790bbb852a2bba4746e37fb4a
Mbaoma Chioma (Omarrr) <mmaryraphaella@gmail.com>
2020-09-05 18:28:03 +0100

    questions 10-13

---
+++ Phase-3/Python Challenge/Day-88.py
@@ -0,0 +1,15 @@
+'''
+1. Write a Python program to find three numbers from an array such that the sum of three numbers equal to a given number
+Input : [1, 0, -1, 0, -2, 2], 0)
+Output : [[-2, -1, 1, 2], [-2, 0, 0, 2], [-1, 0, 0, 1]]
+
+2. Write a Python program to compute and return the square root of a given 'integer'.
+Input : 16
+Output : 4
+Note : The returned value will be an 'integer'
+
+3. Write a Python program to find the single number in a list that doesn't occur twice
+Input : [5, 3, 4, 3, 4]
+Output : 5
+
+'''
