b8cfed07faf02eb0736bc1879955b87bca87a1ac
Mbaoma Chioma (Omarrr) <mmaryraphaella@gmail.com>
2020-09-09 12:17:07 +0100

    Questions 14-16

---
+++ Phase-3/Python Challenge/Day-89.py
@@ -0,0 +1,19 @@
+'''
+
+
+1. Write a Python program to find the single element appears once in a list where every element appears four times except for one.
+Input : [1, 1, 1, 2, 2, 2, 3]
+Output : 3
+
+2. Write a Python program to find two elements appear twice in a list where all the other elements appear exactly twice in the list.
+Input : [1, 2, 1, 3, 2, 5]
+Output :[5, 3]
+
+3. Write a Python program to add the digits of a positive integer repeatedly until the result has a single digit.
+Input : 48
+Output : 3
+For example given number is 59, the result will be 5.
+Step 1: 5 + 9 = 14
+Step 1: 1 + 4 = 5
+
+'''
