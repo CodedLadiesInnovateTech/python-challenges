5646e0bb1a6c3ff1571b7cd452eaf6604dc2c22f
Mbaoma Chioma (Omarrr) <mmaryraphaella@gmail.com>
2020-09-12 07:13:02 +0100

    Questions 17-19

---
+++ Phase-3/Python Challenge/Day-90.py
@@ -0,0 +1,23 @@
+'''
+
+1. Write a Python program to find whether it contains an additive sequence or not.
+The additive sequence is a sequence of numbers where the sum of the first two numbers is equal to the third one.
+Sample additive sequence: 6, 6, 12, 18, 30
+In the above sequence 6 + 6 =12, 6 + 12 = 18, 12 + 18 = 30....
+Also, you can split a number into one or more digits to create an additive sequence.
+Sample additive sequence: 66121830
+In the above sequence 6 + 6 =12, 6 + 12 = 18, 12 + 18 = 30....
+Note : Numbers in the additive sequence cannot have leading zeros.
+ 
+2. Write a Python program to reverse the digits of an integer.
+Input : 234
+Input : -234
+Output: 432
+Output : -432
+
+3. Write a Python program to reverse the bits of an integer (32 bits unsigned).
+Input : 1234
+Output : 1260388352
+For example, 1234 represented in binary as 10011010010 and returns 1260388352 which represents in binary as 1001011001000000000000000000000.
+
+'''
