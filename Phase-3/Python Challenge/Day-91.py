6010b5bcfd607825cdcb0a01e693f67071a79185
Mbaoma Chioma (Omarrr) <mmaryraphaella@gmail.com>
2020-09-13 11:20:03 +0100

    Questions 20-22

---
+++ Phase-3/Python Challenge/Day-91.py
@@ -0,0 +1,18 @@
+'''
+
+1. Write a Python program to check a sequence of numbers is an arithmetic progression or not.
+Input : [5, 7, 9, 11]
+Output : True
+In mathematics, an arithmetic progression or arithmetic sequence is a sequence of numbers such that the difference between the consecutive terms is constant.
+For example, the sequence 5, 7, 9, 11, 13, 15 ... is an arithmetic progression with common difference of 2.
+
+2. Write a Python program to check a sequence of numbers is a geometric progression or not.
+Input : [2, 6, 18, 54]
+Output : True
+In mathematics, a geometric progression or geometric sequence is a sequence of numbers where each term after the first is found by multiplying the previous one by a fixed, non-zero number called the common ratio.
+For example, the sequence 2, 6, 18, 54, ... is a geometric progression with common ratio 3. Similarly, 10, 5, 2.5, 1.25, ... is a geometric sequence with common ratio 1/2.
+
+3. Write a Python program to compute the sum of the two reversed numbers and display the sum in reversed form.
+Input : 13, 14
+Output : 27
+'''
