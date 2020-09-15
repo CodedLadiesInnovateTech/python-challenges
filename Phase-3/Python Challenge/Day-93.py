9b59027609b5fc46822e8454c66aa1b14630f1c3
Mbaoma Chioma (Omarrr) <mmaryraphaella@gmail.com>
2020-09-15 18:15:27 +0100

    Questions 26-28

---
+++ Phase-3/Python Challenge/Day-93.py
@@ -0,0 +1,13 @@
+'''
+1. Write a Python program to check if a given string is an anagram of another given string.
+Input : 'anagram','nagaram'
+Output : True
+According to Wikipedia an anagram is direct word switch or word play, the result of rearranging the letters of a word or phrase to produce a new word or phrase, using all the original letters exactly once; for example, the word anagram can be rearranged into nag-a-ram
+
+2. Write a Python program to push all zeros to the end of a list.
+Input : [0,2,3,4,6,7,10]
+Output : [2, 3, 4, 6, 7, 10, 0]
+
+3. Write a Python program to the push the first number to the end of a list. 
+
+'''
