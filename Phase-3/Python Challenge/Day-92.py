fae12c777d774120c3b888e8786b0a6d853be415
Mbaoma Chioma (Omarrr) <mmaryraphaella@gmail.com>
2020-09-15 10:30:11 +0100

    Questions 23-25

---
+++ Phase-3/Python Challenge/Day-92.py
@@ -0,0 +1,26 @@
+'''
+1. Write a Python program where you take any positive integer n, if n is even, divide it by 2 to get n / 2. If n is odd, multiply it by 3 and add 1 to obtain 3n + 1. Repeat the process until you reach 1.
+Input : 12
+Output : [12, 6.0, 3.0, 10.0, 5.0, 16.0, 8.0, 4.0, 2.0, 1.0]
+According to Wikipedia, the Collatz conjecture is a conjecture in mathematics named after Lothar Collatz, who first proposed it in 1937. The conjecture is also known as the 3n + 1 conjecture.
+The conjecture can be summarized as follows. Take any positive integer n. If n is even, divide it by 2 to get n / 2. If n is odd, multiply it by 3 and add 1 to obtain 3n + 1. Repeat the process (which has been called "Half Or Triple Plus One") indefinitely. The conjecture is that no matter what number you start with, you will always eventually reach 1.
+Example :
+For instance, starting with n = 12, one gets the sequence 12, 6, 3, 10, 5, 16, 8, 4, 2, 1.
+n = 19, for example, takes longer to reach 1: 19, 58, 29, 88, 44, 22, 11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1.
+
+2. Write a Python program to check whether a given number is an ugly number.
+Input : 12
+Output : True
+Ugly numbers are positive numbers whose only prime factors are 2, 3 or 5. The sequence 1, 2, 3, 4, 5, 6, 8, 9, 10, 12, ...
+shows the first 10 ugly numbers.
+Note: 1 is typically treated as an ugly number
+
+3. Write a Python program to get the Hamming numbers upto a given numbers also check whether a given number is an Hamming number.
+Input : 7
+Output : 0
+Hamming numbers are numbers of the form
+H = 2i x 3j x 5k
+Where i, j, k = 0
+The sequence of Hamming numbers 1, 2, 3, 4, 5, 6, 8, 9, 10, 12, 15, 16, 18, 20, 24, 25, 27. . . consists of all numbers of the form 2i.3j.5k where i, j and k are non-negative integers.
+
+'''
