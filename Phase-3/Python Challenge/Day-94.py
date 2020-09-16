225c25f0bb5c55d541a7a33c827ceadf1991e560
Mbaoma Chioma (Omarrr) <mmaryraphaella@gmail.com>
2020-09-16 17:38:39 +0100

    Questions 29-31

---
+++ Phase-3/Python Challenge/Day-94.py
@@ -0,0 +1,16 @@
+'''
+
+1. Write a Python program to find majority element in a list.
+Input : [1, 2, 3, 4, 5, 5, 5, 5, 5, 5, 6]
+Output : 5
+Note: The majority element is the element that appears more than n/2 times where n is the number of elements in the list.
+
+2. Write a Python program to find the length of the last word.
+Input : Python Exercises
+Output : 9
+
+3. Write a Python program to add two binary numbers.
+Input : ('11', '1')
+Output : 100
+
+'''
