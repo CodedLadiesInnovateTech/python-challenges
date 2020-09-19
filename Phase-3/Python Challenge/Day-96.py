9175b91ab2db686b7e0f152f13d102676e757a0d
Mbaoma Chioma (Omarrr) <mmaryraphaella@gmail.com>
2020-09-19 09:51:58 +0100

    Questions 35-37

--- Phase-3/Python Challenge/Day-96.py
+++ Phase-3/Python Challenge/Day-96.py
@@ -4,8 +4,12 @@
 According Wikipedia - A palindromic number or numeral palindrome is a number that remains the same when its digits are reversed. Like 16461, for example, it is "symmetrical". The term palindromic is derived from palindrome, which refers to a word (such as rotor or racecar) whose spelling is unchanged when its letters are reversed. The first 30 palindromic numbers (in decimal) are: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 22, 33, 44, 55, 66, 77, 88, 99, 101, 111, 121, 131, 141, 151, 161, 171, 181, 191, 202,...
 The largest palindrome made from the product of two 3-digit numbers is 913 * 993 = 906609.
 Note: 9999 * 9901 = 906609
+
 2. Write a Python program to find the smallest positive number that is evenly divisible by all of the numbers from 1 to 30.
 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 Result: 2329089562800.0
 
+3. Write a Python program to find the smallest positive number that is evenly divisible by all of the numbers from 1 to 30.
+2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+Result: 2329089562800.0
 ''' 
