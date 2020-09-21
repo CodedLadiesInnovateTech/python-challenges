9d42d44ed0c6e400a6aedfc8dc99968fb059ca91
Mbaoma Chioma (Omarrr) <mmaryraphaella@gmail.com>
2020-09-21 21:01:17 +0100

    Questions 38-40

---
+++ Phase-3/Python Challenge/Day-97.py
@@ -0,0 +1,17 @@
+'''
+
+
+1. Write a python program to find the difference between the sum of the squares of the first two hundred natural numbers and the square of the sum.
+The sum of the squares of the first twenty natural numbers is,
+12+22+32+.....+202 = 2870
+The square of the sum of the first twenty natural numbers is,
+(1 + 2 + ... + 10)2 = 44100
+Hence the difference between the sum of the squares of the first twenty natural numbers and the square of the sum is 44100 - 2870 = 41230 Output: 401323300
+
+2. Write a python program to find the 1000th prime number.
+A prime number is a natural number greater than 1 that has no positive divisors other than 1 and itself. By Euclid's theorem, there are an infinite number of prime numbers. Subsets of the prime numbers may be generated with various formulas for primes. The first twenty prime numbers are: 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71.
+
+3. Write a Python program to find the product xyz.
+A Pythagorean triple consists of three positive integers a, b, and c, such that a2 + b2 = c2. Such a triple is commonly written (a, b, c), and a well-known example is (3, 4, 5). There exists exactly one Pythagorean triplet for which x + y + z = 1000.
+
+'''
