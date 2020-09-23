621a908cf8760e8835401bac0f3f777c812afefc
Mbaoma Chioma (Omarrr) <mmaryraphaella@gmail.com>
2020-09-23 20:29:23 +0100

    questions 1-3

---
+++ Phase-3/Python Class/Day-99.py
@@ -0,0 +1,7 @@
+'''
+1. Write a Python class to convert an integer to a roman numeral.
+2. Write a Python class to convert a roman numeral to an integer.
+3. Write a Python class to find validity of a string of parentheses, '(', ')', '{', '}', '[' and ']. 
+These brackets must be close in the correct order, for example "()" and "()[]{}" are valid but "[)", "({[)]" and "{{{" are invalid.
+
+'''
