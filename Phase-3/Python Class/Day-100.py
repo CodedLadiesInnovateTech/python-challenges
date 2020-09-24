22ce26c68fea6c7c243ada831e47c52e27a62127
Mbaoma Chioma (Omarrr) <mmaryraphaella@gmail.com>
2020-09-24 23:47:07 +0100

    Questions 4-6

---
+++ Phase-3/Python Class/Day-100.py
@@ -0,0 +1,15 @@
+'''
+
+1. Write a Python class to get all possible unique subsets from a set of distinct integers.
+Input : [4, 5, 6]
+Output : [[], [6], [5], [5, 6], [4], [4, 6], [4, 5], [4, 5, 6]]
+
+2. Write a Python class to find a pair of elements (indices of the two numbers) from a given array whose sum equals a specific target number.
+Input: numbers= [10,20,10,40,50,60,70], target=50
+Output: 3, 4
+
+3. Write a Python class to find the three elements that sum to zero from a set of n real numbers.
+Input array : [-25, -10, -7, -3, 2, 4, 8, 10]
+Output : [[-10, 2, 8], [-7, -3, 10]]
+
+'''
